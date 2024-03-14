f7c0efae18bd9a386503362b05eb44614733947a
Louis-Hoang <hoang_t1@denison.edu>
2024-03-14 16:15:54 -0400

    test change name

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,75 +6,74 @@ import linkedin from "../assets/images/linkedin.svg";
 import envelope from "../assets/images/envelope.svg";
 
 function Contact() {
-    const email = () => {
-        window.open("mailto:tung.xuan.h@gmail.com", "_blank");
-    };
-    const github = () => {
-        window.open("https://github.com/Louis-Hoang/portfolio", "_blank");
-    };
-    return (
-        <div className="contact">
-            <FadeInSection>
-                <h2 className="next-step">What's Next?</h2>
-                <h2 className="title">Get In Touch</h2>
-                <div className="contact-text">
-                    Feel free to connnect with me. I am always more than happy
-                    to having insightful conversation, ideas, as well as job
-                    opportunities.
-                </div>
-                <div className="container-button" onClick={email}>
-                    Say Hello
-                </div>
-                <div className="sidebar-mobi">
-                    <div className="social-medias">
-                        <div className="socialmedia">
-                            <a
-                                className="icon"
-                                href="https://drive.google.com/file/d/1ts_HRYOfPs0hPWFPGcgQI_Fu83DmFRaq/view?usp=sharing"
-                                target="_blank"
-                                rel="noreferrer"
-                            >
-                                <img src={resume} alt="resume" />
-                            </a>
-                        </div>
-                        <div className="socialmedia">
-                            <a
-                                className="icon"
-                                href="https://github.com/Louis-Hoang"
-                                target="_blank"
-                                rel="noreferrer"
-                            >
-                                <img src={githubWeb} alt="github" />
-                            </a>
-                        </div>
-                        <div className="socialmedia">
-                            <a
-                                className="icon"
-                                href="https://www.linkedin.com/in/hoang-xtung/"
-                                target="_blank"
-                                rel="noreferrer"
-                            >
-                                <img src={linkedin} alt="linkedin" />
-                            </a>
-                        </div>
-                        <div className="socialmedia">
-                            <a
-                                className="icon"
-                                href="mailto:tung.xuan.h@gmail.com"
-                                target="_blank"
-                                rel="noreferrer"
-                            >
-                                <img src={envelope} alt="email" />
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div className="claim" onClick={github}>
-                    Designed & Built by Tung Hoang
-                </div>
-            </FadeInSection>
+  const email = () => {
+    window.open("mailto:tung.xuan.h@gmail.com", "_blank");
+  };
+  const github = () => {
+    window.open("https://github.com/Louis-Hoang/portfolio", "_blank");
+  };
+  return (
+    <div className="contact">
+      <FadeInSection>
+        <h2 className="next-step">What's Next?</h2>
+        <h2 className="title">Get In Touch</h2>
+        <div className="contact-text">
+          free to connnect with me. I am always more than happy to having
+          insightful conversation, ideas, as well as job opportunities.
         </div>
-    );
+        <div className="container-button" onClick={email}>
+          Say Hello
+        </div>
+        <div className="sidebar-mobi">
+          <div className="social-medias">
+            <div className="socialmedia">
+              <a
+                className="icon"
+                href="https://drive.google.com/file/d/1ts_HRYOfPs0hPWFPGcgQI_Fu83DmFRaq/view?usp=sharing"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={resume} alt="resume" />
+              </a>
+            </div>
+            <div className="socialmedia">
+              <a
+                className="icon"
+                href="https://github.com/Louis-Hoang"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={githubWeb} alt="github" />
+              </a>
+            </div>
+            <div className="socialmedia">
+              <a
+                className="icon"
+                href="https://www.linkedin.com/in/hoang-xtung/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={linkedin} alt="linkedin" />
+              </a>
+            </div>
+            <div className="socialmedia">
+              <a
+                className="icon"
+                href="mailto:tung.xuan.h@gmail.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={envelope} alt="email" />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="claim" onClick={github}>
+          Designed & Built by Tung Hoang
+        </div>
+      </FadeInSection>
+    </div>
+  );
 }
 
 export default Contact;
